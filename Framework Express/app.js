c37db04566305a0130b9d2a3cc53732783c3f68c
Emmanuel Crispin <chidory_15@hotmail.com>
2022-10-17 22:42:15 -0500

    Se agregan los Routers para la consulta de cursos de programacion

--- Framework Express/app.js
+++ Framework Express/app.js
@@ -1,30 +1,32 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const {infoCursos} =  require('./cursos');
+const {infoCursos} =  require('./datos/cursos');
+const routerMatematicas = require('./routers/matematicas.js');
+const routerProgramacion = require('./routers/programacion.js')
 
+//Routers
+app.use('/api/cursos/programacion/', routerProgramacion);
+app.use('/api/cursos/matematicas',routerMatematicas);
 
 //Routing
 app.get('/',(req, res) => {
     res.send('The server is online. Cursos ')
 });
 
+//Obtiene todos los cursos
 app.get('/api/cursos', (req,res) => {
     res.send(JSON.stringify(infoCursos));
 });
 
-app.get('/api/cursos/programacion', (req, res) => {
-    res.send(JSON.stringify(infoCursos.programacion));
-});
-
-app.get('/api/cursos/matematicas', (req, res) => {
-    res.send(JSON.stringify(infoCursos.matematicas));
-});
-
+//Setea el puerto por el que escucha el server
 app.listen(PORT, () => {
     cl(`El servidor esta escuchando en el puerto ${PORT}`)
 });
 
+//Muestra los mensajes en consola
 function cl(obj){
     return console.log(obj);
 }
+
